df76861a4f126e99054bb6ef4f660f729aa66656
Daniel Pozzan <103948626+pozzandaniel@users.noreply.github.com>
2022-09-12 20:13:57 +0200

    add item function

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 $(function() {
-    $("div#container").text("Hello World");
+    $("div#container").append("<h2>To Do List</h2>");
+    $("div#container").append("<input>");
+    $("input").attr({
+        id: "inputField",
+        placeholder: "write here something"
+    });
+    $("div#container").append("<button>Add to the list</button>");
+    $("div#container button").attr("id", "btnAction");
+    $("#btnAction").css({
+        cursor: "pointer"
+    })
+    $("div#container").append("<ul class='list-container'></ul>")
+    $("#btnAction").click(function() {
+        let value = $("#inputField").val();
+        if(value) {
+            let id = Date.now()
+            $("ul").append("<li id="+ id +">"+ value +"</li>")
+            $("#inputField").val("");
+            
+
+        }
+    })
+    
+        
+
+
 })
